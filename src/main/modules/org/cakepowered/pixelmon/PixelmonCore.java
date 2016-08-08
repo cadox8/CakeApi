e969ba45e9454af651c9b204ec5bd393268c26a6
Pablo González <pagoru@gmail.com>
2016-08-08 21:33:55 +0200

    server side only

--- src/main/modules/org/cakepowered/pixelmon/PixelmonCore.java
+++ src/main/modules/org/cakepowered/pixelmon/PixelmonCore.java
@@ -4,7 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(name = "PixelMon|Module", modid = "pixelmon_module", version = "1.0.0")
+@Mod(name = "PixelMon|Module", modid = "pixelmon_module", version = "1.0.0", serverSideOnly = true)
 public class PixelmonCore {
 
 	@Mod.EventHandler
