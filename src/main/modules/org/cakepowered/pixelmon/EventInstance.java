38019592cbad3adce73c04d344a323192da9688c
cout970 <thecout970@gmail.com>
2016-08-09 04:37:20 +0200

    Fix events
    Signed-off-by: cout970 <thecout970@gmail.com>

--- src/main/modules/org/cakepowered/pixelmon/EventInstance.java
+++ src/main/modules/org/cakepowered/pixelmon/EventInstance.java
@@ -1,120 +1,128 @@
 package org.cakepowered.pixelmon;
 
-import com.google.common.eventbus.Subscribe;
+import com.pixelmonmod.pixelmon.api.events.*;
+import com.pixelmonmod.pixelmon.api.events.EconomyEvent.GetBalanceEvent;
+import com.pixelmonmod.pixelmon.api.events.EconomyEvent.PostTransactionEvent;
+import com.pixelmonmod.pixelmon.api.events.EconomyEvent.PreTransactionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.cakepowered.mod.util.ForgeInterface;
+import org.cakepowered.api.core.References;
+import org.cakepowered.api.event.Event;
 import org.cakepowered.pixelmon.events.*;
 
-import com.pixelmonmod.pixelmon.api.events.*;
-import com.pixelmonmod.pixelmon.api.events.EconomyEvent.*;
-
 public enum EventInstance {
-	
-	INSTANCE;
-	
-	//EconomyEvent.class
-	@SubscribeEvent
-	public void on(EconomyEvent e){
-		new PEconomyEvent(e);
-	}
-	@SubscribeEvent
-	public void on(GetBalanceEvent e){
-		new PGetBalanceEvent(e);
-	}
-	@SubscribeEvent
-	public void on(PreTransactionEvent e){
-		new PPreTransactionEvent(e);
-	}
-	@SubscribeEvent
-	public void on(PostTransactionEvent e){
-		new PPostTransactionEvent(e);
-	}
 
-	
-	
-	//BattleStartedEvent
-	@SubscribeEvent
-	public void on(BattleStartedEvent e){
-		new PBattleStartedEvent(e);
-	}
-	//PlayerBattleEndedEvent.class
-	@SubscribeEvent
-	public void on(PlayerBattleEndedEvent e){
-		new PPlayerBattleEndedEvent(e);
-	}
-	//PlayerBattleEndedAbnormalEvent.class
-	@SubscribeEvent
-	public void on(PlayerBattleEndedAbnormalEvent e){
-		new PPlayerBattleEndedEvent(e);
-	}
-	
-	
-	
-	//LostToTrainerEvent.class
-	@SubscribeEvent
-	public void on(LostToTrainerEvent e){
-		new PTrainerBattleEndedEvent(e);
-	}
-	//BeatTrainerEvent.class
-	@SubscribeEvent
-	public void on(BeatTrainerEvent e){
-		new PTrainerBattleEndedEvent(e);
-	}
-	
-	
-	
-	//LevelUpEvent.class
-	@SubscribeEvent
-	public void on(LevelUpEvent e){
-		new PLevelUpEvent(e);
-	}
-	
-	//RareCandyEvent.class
-	@SubscribeEvent
-	public void on(RareCandyEvent e){
-		new PRareCandyEvent(e);
-	}
-	
-	//PixelmonCaptureEvent.class
-	@SubscribeEvent
-	public void on(PixelmonCaptureEvent e){
-		new PPixelmonCaptureEvent(e);
-	}
-	
-	//PixelmonSendOutEvent.class
-	@SubscribeEvent
-	public void on(PixelmonSendOutEvent e){
-		new PPixelmonSendOutEvent(e);
-	}
-	
-	//PixelmonSpawnEvent.class
-	@SubscribeEvent
-	public void on(PixelmonSpawnEvent e){
-		new PPixelmonSpawnEvent(e);
-	}
-	
-	//PixelmonStructureGenerationEvent.class
-	@SubscribeEvent
-	public void on(PixelmonStructureGenerationEvent e){
-		new PPixelmonStructureGenerationEvent(e);
-	}
-	
-	//PlayerActivateShrineEvent.class
-	@SubscribeEvent
-	public void on(PlayerActivateShrineEvent e){
-		new PPlayerActivateShrineEvent(e);
-	}
-	
-	//RidePokemonEvent.class
-	@SubscribeEvent
-	public void on(RidePokemonEvent e){
-		new PRidePokemonEvent(e);
-	}
-	
-	//ThrowPokeballEvent.class
-	@SubscribeEvent
-	public void on(ThrowPokeballEvent e){
-		new PThrowPokeballEvent(e);
-	}
-	
+    INSTANCE;
+
+    private void post(Event e) {
+        References.GAME.getEventRegistry().postEvent(e);
+    }
+
+    //EconomyEvent.class
+    @SubscribeEvent
+    public void on(EconomyEvent e) {
+        post(new PEconomyEvent(e));
+    }
+
+    @SubscribeEvent
+    public void on(GetBalanceEvent e) {
+        post(new PGetBalanceEvent(e));
+    }
+
+    @SubscribeEvent
+    public void on(PreTransactionEvent e) {
+        post(new PPreTransactionEvent(e));
+    }
+
+    @SubscribeEvent
+    public void on(PostTransactionEvent e) {
+        post(new PPostTransactionEvent(e));
+    }
+
+
+    //BattleStartedEvent
+    @SubscribeEvent
+    public void on(BattleStartedEvent e) {
+        post(new PBattleStartedEvent(e));
+    }
+
+    //PlayerBattleEndedEvent.class
+    @SubscribeEvent
+    public void on(PlayerBattleEndedEvent e) {
+        post(new PPlayerBattleEndedEvent(e));
+    }
+
+    //PlayerBattleEndedAbnormalEvent.class
+    @SubscribeEvent
+    public void on(PlayerBattleEndedAbnormalEvent e) {
+        post(new PPlayerBattleEndedEvent(e));
+    }
+
+
+    //LostToTrainerEvent.class
+    @SubscribeEvent
+    public void on(LostToTrainerEvent e) {
+        post(new PTrainerBattleEndedEvent(e));
+    }
+
+    //BeatTrainerEvent.class
+    @SubscribeEvent
+    public void on(BeatTrainerEvent e) {
+        post(new PTrainerBattleEndedEvent(e));
+    }
+
+
+    //LevelUpEvent.class
+    @SubscribeEvent
+    public void on(LevelUpEvent e) {
+        post(new PLevelUpEvent(e));
+    }
+
+    //RareCandyEvent.class
+    @SubscribeEvent
+    public void on(RareCandyEvent e) {
+        post(new PRareCandyEvent(e));
+    }
+
+    //PixelmonCaptureEvent.class
+    @SubscribeEvent
+    public void on(PixelmonCaptureEvent e) {
+        post(new PPixelmonCaptureEvent(e));
+    }
+
+    //PixelmonSendOutEvent.class
+    @SubscribeEvent
+    public void on(PixelmonSendOutEvent e) {
+        post(new PPixelmonSendOutEvent(e));
+    }
+
+    //PixelmonSpawnEvent.class
+    @SubscribeEvent
+    public void on(PixelmonSpawnEvent e) {
+        post(new PPixelmonSpawnEvent(e));
+    }
+
+    //PixelmonStructureGenerationEvent.class
+    @SubscribeEvent
+    public void on(PixelmonStructureGenerationEvent e) {
+        post(new PPixelmonStructureGenerationEvent(e));
+    }
+
+    //PlayerActivateShrineEvent.class
+    @SubscribeEvent
+    public void on(PlayerActivateShrineEvent e) {
+        post(new PPlayerActivateShrineEvent(e));
+    }
+
+    //RidePokemonEvent.class
+    @SubscribeEvent
+    public void on(RidePokemonEvent e) {
+        post(new PRidePokemonEvent(e));
+    }
+
+    //ThrowPokeballEvent.class
+    @SubscribeEvent
+    public void on(ThrowPokeballEvent e) {
+        post(new PThrowPokeballEvent(e));
+    }
+
 }
