46698a5c2215cfbf6f7af48c909c2a9abd7b764b
Pablo González <pagoru@gmail.com>
2016-09-07 17:20:14 +0200

    Change ThrowPokeballEvent
    Added getPlayer

--- src/main/modules/org/cakepowered/pixelmon/events/PThrowPokeballEvent.java
+++ src/main/modules/org/cakepowered/pixelmon/events/PThrowPokeballEvent.java
@@ -1,6 +1,8 @@
 package org.cakepowered.pixelmon.events;
 
+import org.cakepowered.api.entity.Player;
 import org.cakepowered.mod.event.ApiEvent;
+import org.cakepowered.mod.util.ForgeInterface;
 import org.cakepowered.pixelmon.enums.PPokeball;
 
 import com.pixelmonmod.pixelmon.api.events.ThrowPokeballEvent;
@@ -17,7 +19,11 @@ public class PThrowPokeballEvent extends ApiEvent {
 	public PPokeball getPokeballType(){
 		return PPokeball.valueOf(e.type.name());
 	}
-	
+
+	public Player getPlayer(){
+		return ForgeInterface.getPlayer(e.player);
+	}
+
 	public void setCanceled(boolean cancel){
 		e.setCanceled(cancel);
 	}
